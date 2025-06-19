6180a4c0346f313269534de0461c3c62da86029e
jay menorca <zaimenorca@gmail.com>
2025-06-18 21:31:00 -0400

    section4.consumergroup.KafkaConsumer/KafkaProducer/KafkaConsumerGroup; setup topic (order-events) with 3 partitions

---
+++ reactive-kafka-sandbox/src/main/java/jayslabs/kafka/section4/consumergroup/KafkaConsumer.java
@@ -0,0 +1,48 @@
+package jayslabs.kafka.section4.consumergroup;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+public class KafkaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+    public static void start(String instanceId) {
+
+        var consumerConfig = Map.<String, Object>of(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.GROUP_ID_CONFIG, "inventory-service-group",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+            ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, instanceId,
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true
+        );
+
+        var receiverOptions = ReceiverOptions
+        .create(consumerConfig)
+        //.subscription(Pattern.compile("order.*"));
+        .subscription(List.of("order-events"));
+
+
+        KafkaReceiver.create(receiverOptions)
+            .receive()
+            .doOnNext(record -> {
+                //acknowledge the message
+                log.info("Received message - topic: {}, partition: {}, offset: {}, key: {}, value: {}", 
+                record.topic(), record.partition(), record.offset(), record.key(), record.value());
+
+            })
+            .doOnNext(record -> record.receiverOffset().acknowledge())
+            .subscribe();
+
+
+    }
+
+}
