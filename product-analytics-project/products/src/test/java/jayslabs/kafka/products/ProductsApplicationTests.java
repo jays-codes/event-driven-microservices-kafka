e9f372d27137c3cf2c9a85d353bec2d6becda494
jay menorca <zaimenorca@gmail.com>
2025-07-24 14:10:22 -0400

    product-analytics-project/products: created integration test components: ProductsApplicationTests (extends <<AbstractIntegrationTest>>); uses WebTestClient

--- product-analytics-project/products/src/test/java/jayslabs/kafka/products/ProductsApplicationTests.java
+++ product-analytics-project/products/src/test/java/jayslabs/kafka/products/ProductsApplicationTests.java
@@ -1,13 +1,50 @@
 package jayslabs.kafka.products;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import jayslabs.kafka.products.event.ProductViewEvent;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
-class ProductsApplicationTests {
+@AutoConfigureWebTestClient
+class ProductsApplicationTests extends AbstractIntegrationTest {
+
+    @Autowired
+	private WebTestClient webclient;
 
 	@Test
-	void contextLoads() {
+	void testProductViewAndEvents() {
+		//view products
+		viewProduct(1);
+		viewProduct(1);
+		viewProduct(5);
+
+		//check if events are available in the topic
+		var flux = this.<ProductViewEvent>createReceiver(PRODUCT_VIEW_EVENTS)
+		.receive()
+		.take(3);
+
+		StepVerifier.create(flux)
+		.consumeNextWith(rr -> Assertions.assertEquals(1, rr.value().getProductId()))
+		.consumeNextWith(rr -> Assertions.assertEquals(1, rr.value().getProductId()))
+		.consumeNextWith(rr -> Assertions.assertEquals(5, rr.value().getProductId()))
+		.verifyComplete();
+	}
+
+	private void viewProduct(int productId) {
+		webclient
+		.get()
+		.uri("/product/" + productId)
+		.exchange()
+		.expectStatus().is2xxSuccessful()
+		.expectBody()
+		.jsonPath("$.id").isEqualTo(productId)
+		.jsonPath("$.description").isEqualTo("product-" + productId);
 	}
 
 }
